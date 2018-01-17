ed16a350c0371040b8e43a78a89acaa571d1f01f
Ka Chun Yung <kcyung780@hotmail.com>
2018-01-17 00:22:18 -0700

    Comments

--- WordsManipulation/Form1.cs
+++ WordsManipulation/Form1.cs
@@ -18,8 +18,8 @@ namespace WordsManipulation
         private delegate void delVoidInt(int n);
         private delegate string delStringVoid();
 
-        List<string> _words; // Master list of words
-        Thread _thread = null;
+        List<string> _words;        // Master list of words
+        Thread _thread = null;      // Variable to check if a thread is running
         System.Diagnostics.Stopwatch _sw = new System.Diagnostics.Stopwatch();
 
         public WordsThread()
@@ -38,9 +38,12 @@ namespace WordsManipulation
             {
                 UI_TB_Output.Text = "Error reading from file";
             }
+
             UI_Label.Text = "Loaded " + _words.Count().ToString() + " words!";
         }
 
+
+        // *************    EVENT HANDLERS     ************* //
         private void UI_BTN_Palindromes_Click(object sender, EventArgs e)
         {
             // Check if a thread is currently running
@@ -49,19 +52,17 @@ namespace WordsManipulation
                 UI_TB_Output.Text = "Program is running, please wait";
                 return;
             }
-            else
-            {
-                // update UI
-                UI_TB_Output.Text = "Palindrome search in progress...";
-                UI_BTN_ASCII.Enabled = false;
-                UI_BTN_Substring.Enabled = false;
 
-                // start parametized thread start with distinct word list
-                _thread = new Thread(Palindromes);
-                _thread.IsBackground = true;
-                _thread.Start(DistinctLowerCase(_words));
-                _sw.Restart();
-            }         
+            // update UI
+            UI_TB_Output.Text = "Palindrome search in progress...";
+            UI_BTN_ASCII.Enabled = false;
+            UI_BTN_Substring.Enabled = false;
+
+            // start parametized thread start with distinct word list
+            _thread = new Thread(Palindromes);
+            _thread.IsBackground = true;
+            _thread.Start(DistinctLowerCase(_words));
+            _sw.Restart();
         }
 
         private void UI_BTN_ASCII_Click(object sender, EventArgs e)
@@ -71,17 +72,15 @@ namespace WordsManipulation
                 UI_TB_Output.Text = "Program is running, please wait";
                 return;
             }
-            else
-            {
-                UI_TB_Output.Text = "ASCII search is in progress...";
-                UI_BTN_Palindromes.Enabled = false;
-                UI_BTN_Substring.Enabled = false;
 
-                _thread = new Thread(ASCII);
-                _thread.IsBackground = true;
-                _thread.Start(_words);
-                _sw.Restart();
-            }
+            UI_TB_Output.Text = "ASCII search is in progress...";
+            UI_BTN_Palindromes.Enabled = false;
+            UI_BTN_Substring.Enabled = false;
+
+            _thread = new Thread(ASCII);
+            _thread.IsBackground = true;
+            _thread.Start(_words);
+            _sw.Restart();
         }
 
         private void UI_BTN_Substring_Click(object sender, EventArgs e)
@@ -108,7 +107,7 @@ namespace WordsManipulation
             _sw.Restart();
         }
 
-        // Support Method
+        // Support Method - returns a distinct list of all words in lower case
         private List<string> DistinctLowerCase(List<string> wordList)
         {
             List<string> distinct = new List<string>();
@@ -119,9 +118,13 @@ namespace WordsManipulation
         // Palindromes Threads
         private void Palindromes(object words)
         {
+            // Cast out distinct lowercase list of words
             List<string> wordList = (List<string>)words;
+
+            // Create a list to store all palindromes
             List<string> palList = new List<string>();
 
+            // Palindrome check for words larger than one character
             foreach(string word in wordList)
             {
                 if (word.Length > 1 && word == new string(word.ToCharArray().Reverse().ToArray()))
@@ -144,20 +147,27 @@ namespace WordsManipulation
         // ASCII Same Thread
         private void ASCII(Object wordList)
         {
+            // Cast out word list
             List<string> words = (List<string>)wordList;
+
+            // Create a list to store first unique ASCII sum
             List<string> AsciiOutput = new List<string>();
+
+            // Create a list to store all found ASCII sums so far
             List<int> AsciiCount = new List<int>();
+
+            // The ASCII sum of current word being checked
             int asciiSum;
             
             foreach(string word in words)
             {
+                // Reset the sum for each new word
                 asciiSum = 0;
-                // find the ascii count
+
+                // Find the ASCII sum for the word
                 foreach (char c in word)
                     asciiSum += (int)c;           
-                // asciiSum = word.Sum(o => o);
-
-                // check if the count exist in AsciiCount list - if not add to both list
+               
                 if (!AsciiCount.Contains(asciiSum))
                 {
                     AsciiCount.Add(asciiSum);
@@ -168,6 +178,8 @@ namespace WordsManipulation
             AsciiOutput.Sort();
 
             WriteToOutputFile(AsciiOutput, "same.txt");
+
+            // Update the UI
             try
             {
                 Invoke(new delVoidInt(UpdateUI), AsciiOutput.Count);
@@ -181,11 +193,16 @@ namespace WordsManipulation
         // Substring Thread
         private void Substring(object distinctList)
         {
+            // Cast out distinct list of lower case words
             List<string> words = (List<string>)distinctList;
+
+            // Create a list to store all matching substrings
             List<string> subList = new List<string>();
 
+            // current substring
             string sub = "";
 
+            // Get the substring from the UI via a delegate
             try
             {
                 sub = (string)Invoke(new delStringVoid(PullString));
@@ -195,10 +212,12 @@ namespace WordsManipulation
                 System.Diagnostics.Trace.WriteLine("Target thread is dead");
             }
 
+            // Iterate through list and word to substring list if it matches
             words.ForEach(o => { if (o.Contains(sub)) subList.Add(o); });
 
             WriteToOutputFile(subList, "substring.txt");
 
+            // Update the UI
             try
             {
                 Invoke(new delVoidInt(UpdateUI), subList.Count);
@@ -212,9 +231,17 @@ namespace WordsManipulation
         // Create text file to write Palindromes/Ascii/Substring list to
         private void WriteToOutputFile(List<string> outputList, string filename)
         {
-            File.WriteAllLines(filename, outputList);
+            try
+            {
+                File.WriteAllLines(filename, outputList);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Trace.WriteLine("Error writing to file: " + err);
+            }
         }
 
+        // Method used to update the UI upon completion of palindrome/ASCII sum/substring search
         private void UpdateUI(int listCount)
         {
             _sw.Stop();
@@ -238,6 +265,7 @@ namespace WordsManipulation
             _thread = null;
         }
 
+        // Method for the delegate to supply substring thread with the textbox input
         private string PullString()
         {
             return UI_TB_Substring.Text.Trim().ToLower();
